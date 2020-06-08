fcbc55d039a71b8440a98c173e05fd43ec2b1a68
isitfall <PDAnd4@yandex.ru>
2020-06-09 00:02:22 +0300

    first quest completed! changed sections:  header, info, services

--- projectOrthopedy/js/script.js
+++ projectOrthopedy/js/script.js
@@ -39,19 +39,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //slow scroll
 
-    const anchors = document.querySelectorAll('a[href*="#"]')
+        const anchors = document.querySelectorAll('a[href*="#"]')
 
-    for (let anchor of anchors) {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault()
-        
-        const blockID = anchor.getAttribute('href').substr(1)
-        
-        document.getElementById(blockID).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
+        for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+            
+            const blockID = anchor.getAttribute('href').substr(1)
+            
+            document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+            })
         })
-    })
-}
+    }
+
 
 });
